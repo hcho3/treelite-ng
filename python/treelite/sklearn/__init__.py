8d1d37dbe7b65b8c85c00888194e91efdfe3b8a6
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-18 19:35:21 -0700

    Add round trip with serializer

--- python/treelite/sklearn/__init__.py
+++ python/treelite/sklearn/__init__.py
@@ -4,16 +4,11 @@ from .importer import import_model
 
 
 def import_model_with_model_builder(sklearn_model):
-    """
-    Load a tree ensemble model from a scikit-learn model object using the model builder API.
-
-    .. note:: Use ``import_model`` for production use
-
-        This function exists to demonstrate the use of the model builder API and is slow with
-        large models. For production, please use :py:func:`~treelite.sklearn.import_model`
-        which is significantly faster.
-    """
-    raise NotImplementedError("Not implemented")
+    """Stub for removed function"""
+    raise NotImplementedError(
+        "treelite.sklearn.import_model_with_model_builder() was removed in Treelite 4.0. "
+        "Please use treelite.sklearn.import_model() instead."
+    )
 
 
 __all__ = ["import_model", "import_model_with_model_builder"]
